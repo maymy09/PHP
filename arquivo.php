9cbbe7d2cdec4747f822c7673c6509c72bdcbfdd
Maysa <139801379+maymy09@users.noreply.github.com>
2023-08-31 16:42:38 -0300

    Add files via upload

---
+++ arquivo.php
@@ -0,0 +1,11 @@
+<?<php
+// Esté u comentario de apenas uma linha
+$nome = "Belo Horizonte";
+$Nome = "Nivaldo";
+$texto = "mora";
+$textoDois = "em";
+$textoTres = "e";
+$idade = 30;
+
+$resposta = $nome . " " . $texto . $textoDois . $cidade . $textoTres
+?>
